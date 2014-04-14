72e43b1be875acfc58a4fb3bc24e88a777e21441
Avenger7x <avenger7x13@gmail.com>
2014-04-14 12:21:33 +0300

    specs

--- spec.js
+++ spec.js
@@ -1,1 +1,15 @@
-// specs
+/* global describe, it, expect */
+'use strict';
+var slugify = require('./index');
+
+describe('slugify', function(){
+	it('should convert spaces to dash', function(){
+		var actual = slugify('hello world'),
+			expected = 'hello-world';
+
+		expect(actual).toBe(expected);
+	});
+
+	// should convert to lower case
+	// should convert camelcase (by option)
+});
